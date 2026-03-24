c22276d4d6fe046540671f1c1e6117cf28085c45
oLucasLeles <lucasleles.ep@gmail.com>
2026-03-24 00:16:59 +0000

    adiciona texto na home

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Fala amor! Te amo! Se você me ama DE VERDADE, me liga imediatamente quando ver essa mensagem.</h1>
+    return <h1>Fala amor! Te amo! Se você me ama DE VERDADE, me liga imediatamente quando ver essa mensagem. Mudancinha leve para fazer um novo commit...</h1>
 }
 
 export default Home;
