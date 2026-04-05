dc861f90a473a293b2dcac0817c2e2d018b4e934
oLucasLeles <lucasleles.ep@gmail.com>
2026-04-05 15:34:18 +0000

    adiciona mensagem carinhosa..

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,5 @@
 function Home() {
-  return (
-    <h1>
-      Fala amor! Te amo! Se você me ama DE VERDADE, me liga imediatamente quando
-      ver essa mensagem. Mudancinha leve para fazer um novo commit...
-    </h1>
-  );
-}
-
-function teste2() {
-  console.log("identação errada");
+  return <h1>O gabriel é extremamente viado</h1>;
 }
 
 export default Home;
